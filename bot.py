04777152b6cdd097e97d623a5488bbc819afadb8
optc-ranking <35119439+optc-ranking@users.noreply.github.com>
2025-06-23 16:50:55 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,7 @@
 import os
 import discord
 from discord.ext import commands
-from flask import Flask
-from threading import Thread
 
-# Flask web server for keep-alive
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "I'm alive!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-# Discord bot setup
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
@@ -34,7 +17,4 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('Pong!')
 
-# Start the keep-alive webserver, then the bot
-if __name__ == "__main__":
-    keep_alive()
-    bot.run(TOKEN)
+bot.run(TOKEN)
